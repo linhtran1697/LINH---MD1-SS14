220710bb8d3ac4635ced9e49f8a87443e71046a6
linhtran1697 <linhtran.1697.lt@gmail.com>
2026-06-01 12:14:43 +0900

    Add files via upload

---
+++ SS14-EX2/SS14-EX2.js
@@ -0,0 +1,22 @@
+// Tạo một đối tượng car với các thuộc tính sau:
+// brand: "Toyota"
+// model: "Corolla"
+// year: 2020
+//Yêu cầu:
+// Thêm một thuộc tính mới là color với giá trị "red"
+//Cập nhật giá trị của thuộc tính year thành 2022
+// In ra đối tượng sau khi cập nhật
+const car = {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+
+// Thêm thuộc tính color
+car.color = "red";
+
+// Cập nhật giá trị của thuộc tính year
+car.year = 2022;
+
+// In ra đối tượng sau khi cập nhật
+console.log(car);
