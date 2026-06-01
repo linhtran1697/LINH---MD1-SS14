1c2f0d2d73bd03a7d89fcf4fba7ed4dddaa7da6b
linhtran1697 <linhtran.1697.lt@gmail.com>
2026-06-01 12:03:45 +0900

    Add files via upload

---
+++ SS14-EX1/SS14-EX1.js
@@ -0,0 +1,17 @@
+// Tạo một đối tượng tên là person có các thuộc tính sau:
+// name: "John Doe"
+//age: 25
+//job: "Developer"
+// Sau đó, sử dụng console  để in ra:
+// Giá trị của thuộc tính name.
+// Tất cả các thông tin của đối tượng.
+
+const person = {
+    name: "John Doe",
+    age: 25,
+    job: "Developer"
+};
+
+console.log(`Họ và tên: ${person.name}`);
+console.log(`Tuổi: ${person.age}`);
+console.log(`Nghề nghiệp: ${person.job}`);
